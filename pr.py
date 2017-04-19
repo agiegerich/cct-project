e2c16f26cec0cbd0b1a828090e043be158f301d5
Albert Giegerich <albert.a.giegerich@gmail.com>
2017-04-19 01:00:59 +0000

    test

--- pr.py
+++ pr.py
@@ -1,4 +1,6 @@
 from operator import add
+import gc
+import sys
 
 
 def parse_int(id):
@@ -14,12 +16,17 @@ def update_weight_if_has_outgoing(weight, outgoing):
     else:
         return weight / outgoing
         
-def distribute_page_rank(dest_id, in_list, score_map):
+def distribute_page_rank(in_list, score_map):
     sum = 0.0
     for src in in_list:
         if src in score_map:
             sum += score_map[src]
     return sum
+
+'''
+def distribute_page_rank(in_list, score_rdd):
+    return score_rdd.filter(lambda (id, weight_io): id in in_list).reduce(lambda (a, b): a[1][0] + b[1][0])
+'''
 
 class PageRankCalculator:
     def __init__(self, page_links_by_source, page_links_by_target, sc):
@@ -47,29 +54,51 @@ class PageRankCalculator:
         self.hard_scores = {}
         # We can either do this as a map or as a List. a List might be better...
         #self.hard_scores = self.page_rank_scores.map(lambda (id, (weight, in_out)): (id, weight) ).collectAsMap()
+        self.init_hard_scores = False
         
 
     def run_page_rank(self, damp_param):
         num_active = self.num_active
-        hard_scores = self.hard_scores
+        
+        '''
+        if self.init_hard_scores:
+            hard_scores_rdd = self.hard_scores_rdd
+        self.init_hard_scores=True
+        '''
+            
         # step 1
+        #CHANGED
         hard_scores = self.page_rank_scores.map(lambda (id, (weight, (in_list, out_list))) : (id, update_weight_if_has_outgoing(weight, len(out_list))) ).collectAsMap()
+        #hard_scores_rdd = self.page_rank_scores.map(lambda (id, (weight, (in_list, out_list))) : (id, (update_weight_if_has_outgoing(weight, len(out_list)), (in_list, out_list))) )
 
         # step 3
         # get the combined page ranks for the articles with no outgoing links to compute the bias
         # weight_io_tuple is (weight, (in_list, out_list))
+
+        #CHANGED
         no_out_bias = self.page_rank_scores.filter(lambda (id, weight_io_tuple): len(weight_io_tuple[1][1]) == 0).map(lambda (id, na): hard_scores[id] if id in hard_scores else 0).reduce(add)
+        #no_out_bias = hard_scores_rdd.filter(lambda (id, weight_io): len(weight_io[1][1]) == 0).map(lambda (id, weight_io): weight_io[1]).reduce(add)
         
         no_out_bias = no_out_bias/num_active
 
         # NOTE: I swapped step 2 and 3 as they don't rely on each other and the temp_page_rank_scores aren't needed until step 4
         # step 2, distribute page rank scores
         # weight_io_tuple is (weight, (in_list, out_list))
-        temp_page_rank_scores = self.page_rank_scores.map(lambda (id, weight_io_tuple): (id, (distribute_page_rank(id, weight_io_tuple[1][0], hard_scores), (weight_io_tuple[1][0], weight_io_tuple[1][1]))) )
+
+        # CHANGED
+        temp_page_rank_scores = self.page_rank_scores.map(lambda (id, weight_io_tuple): (id, (distribute_page_rank(weight_io_tuple[1][0], hard_scores), (weight_io_tuple[1][0], weight_io_tuple[1][1]))) )
+        #temp_page_rank_scores = self.page_rank_scores.map(lambda (id, weight_io): (id, (distribute_page_rank_rdd(weight_io[1][0], hard_scores_rdd), weight_io[1])))
         
         # step 4, 
+
+        # CHANGED
         hard_scores = temp_page_rank_scores.map(lambda (id, weight_io_tuple): (id, weight_io_tuple[0] * damp_param + no_out_bias * damp_param + (1 - damp_param) / num_active ) ).collectAsMap()
+        #hard_scores_rdd = temp_page_rank_scores.map(lambda (id, weight_io): (id, ((weight_io[0] * damp_param + no_out_bias * damp_param + (1 - damp_param) / num_active), weight_io[1] ) ) )
+
+        # CHANGED
         self.hard_scores = hard_scores
+        gc.collect()
+        #self.hard_scores_rdd = hard_scores_rdd
 
 
 def remove_invalid_links(links):
@@ -82,7 +111,7 @@ def remove_invalid_links(links):
 def main(sc): 
     #data_file_full = '/media/WinShare/provisions\ for\ LDA\ server/pagelinks_sparse.csv'
     #data_file = '/media/WinShare/provisions\ for\ LDA\ server/pagelinks_sparse_sample.csv'
-    raw_data = sc.textFile('s3n://agiegerich-lda/pagelinks_sparse_small.csv')
+    raw_data = sc.textFile('s3n://agiegerich-lda/pagelinks_sparse.csv')
     # I added the reduceByKey to the end to make sure all article_ids are unique
     pagelinks_by_target = raw_data.map(lambda l: l.split(",")).map(lambda p:(int(p[0]), map(lambda e: parse_int(''.join([c for c in e if c in '1234567890'])), p[2:]))).reduceByKey(lambda x,y: x+y).filter(lambda (id, links): id != -1)
     pagelinks_by_target = pagelinks_by_target.map(lambda (id, links): (id, remove_invalid_links(links)))
@@ -97,20 +126,7 @@ def main(sc):
     for i in range(iterations):
         pagerank.run_page_rank(0.85)
 
-    sum = 0
-    for (k, v) in pagerank.hard_scores.items():
-        sum += v
-        
-    i = 0
-    take = 20
-    for z in sorted(pagerank.hard_scores.items(), key=lambda x: x[0]):
-        i += 1
-        print(z)
-        if i == take:
-            break
-    print(sum)
-    
-    #sc.parallelize(pagerank.hard_scores)
-
-
-    # The final page rank values are in pagerank.hard_scores
+    sc.parallelize(pagerank.hard_scores.items()).saveAsTextFile(
+        path="s3n://agiegerich-lda/pagelinks_output-full1/",
+        compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec" 
+    )
